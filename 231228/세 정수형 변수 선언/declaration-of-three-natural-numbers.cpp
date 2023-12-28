4814bf3fe6e0d7615862e5a701664ebff98a169e
yunchan312 <71773024+yunchan312@users.noreply.github.com>
2023-12-28 22:11:43 +0900

    Title: 세 정수형 변수 선언, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231228/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    int a,b,c;
+    a=7;
+    b=23;
+    c=30;
+    cout << a<< " + " << b << " = " << c;
+    return 0;
+}
